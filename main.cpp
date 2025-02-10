9787d3e6e115429c30472478b0f1c2979ab84f88
ej19748 <36275886-ej19748@users.noreply.replit.com>
2025-02-10 22:36:52 +0000

    User checkpoint: Implement grade calculation and display functionality.  Replaces rock-paper-scissors game with a grade averaging program.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,74 @@
 #include <iostream>
 #include <string>
-#include <ctime>
-#include <cstdlib>
 
 using namespace std;
 
-int main() {
-  srand(time(0));
-  string userChoice;
-  cout << "Choose rock, paper, or scissors: ";
-  getline(cin, userChoice);
-  string computerChoice;
-  int randomNumber = rand() % 3;
-  switch (randomNumber) {
-    case 0:
-      computerChoice = "rock";
-      break;
-    case 1:
-      computerChoice = "paper";
-      break;
-    case 2:
-      computerChoice = "scissors";
-      break;
-  }
-  cout << "You chose: " << userChoice << endl;
-  cout << "The computer chose: " << computerChoice << endl;
+void readScores(double& s1, double& s2, double& s3);
+void getGrade(double average);
+double getAverage(double s1, double s2, double s3);
+void printAverage(double average);
+void print(double s1, double s2, double s3, double average, char grade);
 
-  if (userChoice == computerChoice) {
-    cout << "It's a tie!" << endl;
-  } else if ((userChoice == "rock" && computerChoice == "scissors") || (userChoice == "paper" && computerChoice == "rock") || (userChoice == "scissors" && computerChoice == "paper")) {
-    cout << "You win!" << endl;
-  } else {
-    cout << "The computer wins!" << endl;
-  }
-  return 0;
+int main() {
+    double s1, s2, s3, average;
+    char grade = 0;
+    readScores(s1, s2, s3);
+    average = getAverage(s1, s2, s3);
+    getGrade(average);
+    print(s1, s2, s3, average, grade);
+    return 0;
 }
+void readScores(double& s1, double& s2, double& s3) {
+    cout << "Enter three grades: ";
+    cin >> s1 >> s2 >> s3;
+    while (s1 < 0)
+      {
+        cout << "Enter a grade from 0 to 100: ";
+        cin >> s1;
+        break;
+      }
+    while (s2 < 0)
+      {
+        cout << "Enter a grade from 0 to 100: ";
+        cin >> s2;
+        break;
+      }
+    while (s3 < 0)
+      {
+        cout << "Enter a grade from 0 to 100: ";
+        cin >> s3;
+        break;
+      }
+    }
+    double getAverage(double s1, double s2, double s3) { 
+    double average = (s1 + s2 + s3) / 3.0;
+    char grade = 0;
+      grade = s1 * s2 * s3;
+    return average;
+  }
+    void getGrade(double average) {
+      cout << "Average: " << average << endl;
+      if (90 < average && average <= 100) {
+        cout << "Grade: A" << endl;
+      }
+      else if (80 < average && average < 89) {
+        cout << "Grade: B" << endl;
+      }
+       else if (70 < average && average < 79) {
+        cout << "Grade: C" << endl;
+      }
+        else if (60 < average && average < 69) {
+        cout << "Grade: D" << endl;
+      }
+        else if (0 < average && average < 59) {
+        cout << "Grade: F" << endl;
+      }
+    } 
+    void print(double s1, double s2, double s3, double average, char grade) {
+      cout << "Score 1: " << s1 << endl;
+      cout << "Score 2: " << s2 << endl;
+      cout << "Score 3: " << s3 << endl;
+      cout << "Average: " << average << endl;
+      cout << "Grade: " << grade << endl;
+    }
+  
