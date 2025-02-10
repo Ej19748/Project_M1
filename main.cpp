b483cc92fb1da1e1e3345f34c14289e1f539df73
ej19748 <36275886-ej19748@users.noreply.replit.com>
2025-02-10 22:59:04 +0000

    User checkpoint: Fix: Correct function signatures and input validation to resolve linker errors and improve grade calculation.

--- main.cpp
+++ main.cpp
@@ -4,36 +4,35 @@
 using namespace std;
 
 void readScores(double& s1, double& s2, double& s3);
-void getGrade(double average);
-double getAverage(double s1, double s2, double s3);
-void printAverage(double average);
-void print(double s1, double s2, double s3, double average, char grade);
+void getGrade(const double s1, const double s2, const double s3, double& avg, char& grade);
+double getAverage(const double s1, const double s2, const double s3, double& average);
+void print(const double s1, const double s2, const double s3, const double average, const char grade);
 
 int main() {
     double s1, s2, s3, average;
     char grade = 0;
     readScores(s1, s2, s3);
-    average = getAverage(s1, s2, s3);
-    getGrade(average);
+    getGrade(s1, s2, s3, average, grade);
+    getAverage(s1, s2, s3, average);
     print(s1, s2, s3, average, grade);
     return 0;
 }
 void readScores(double& s1, double& s2, double& s3) {
     cout << "Enter three grades: ";
     cin >> s1 >> s2 >> s3;
-    while (s1 < 0)
+    while (s1 < 0 || s1 > 100)
       {
         cout << "Enter a grade from 0 to 100: ";
         cin >> s1;
         break;
       }
-    while (s2 < 0)
+    while (s2 < 0 || s2 > 100)
       {
         cout << "Enter a grade from 0 to 100: ";
         cin >> s2;
         break;
       }
-    while (s3 < 0)
+    while (s3 < 0 || s3 > 100)
       {
         cout << "Enter a grade from 0 to 100: ";
         cin >> s3;
@@ -44,7 +43,7 @@ void readScores(double& s1, double& s2, double& s3) {
     double average = (s1 + s2 + s3) / 3.0;
     return average;
   }
-    void getGrade(double average) {
+    void getGrade(double average, char& grade) {
       cout << "Average: " << average << endl;
       if (90 < average && average <= 100) {
         cout << "Grade: A" << endl;
@@ -69,4 +68,3 @@ void readScores(double& s1, double& s2, double& s3) {
       cout << "Average: " << average << endl;
       cout << "Grade: " << grade << endl;
     }
-  
